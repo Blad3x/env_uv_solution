aa0b8c296327a5de758af3c408e160d46efaf419
Cristi M <cristi.23i@gmail.com>
2026-02-16 21:47:10 +0200

    Test yield vs return in the main python script

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def main():
-    print("Hello from env-uv-solution!")
+    yield("Hello from env-uv-solution!")
+    yield("Hello again!")
+    return("Bye")
+    return("Bye again - not shown")
 
 
 if __name__ == "__main__":
-    main()
+    gen = main()
+    try:
+         while True:
+             print(next(gen))
+#        for item in  main():
+#            print(item)
+    except StopIteration as e:
+        print(e.value)
+        print(e.value)
