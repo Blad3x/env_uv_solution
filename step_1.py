14ca231f0bd6185c6cc11a78c64a135ec5ef6f58
Cristi M <cristi.23i@gmail.com>
2026-02-16 23:35:19 +0200

    Al doilea commit pe fix_main branch.

--- step_1.py
+++ step_1.py
@@ -19,7 +19,7 @@ class TextApp(App):
 		
 		# Styling the Label widget
 		self.label.styles.background = "darkgreen"
-		self.label.styles.border = ("double", "red")
+		self.label.styles.border = ("double", "orange")
 		self.label.styles.padding = 1, 1
 		self.label.styles.margin = 2, 4
 		
